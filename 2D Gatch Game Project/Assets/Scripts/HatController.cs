1d035f3c3e8be7990fd6f1c694229bb9e726ee6f
Ivan <S1lveria@users.noreply.github.com>
2017-12-16 19:07:11 -0500

    Add HatController script

---
+++ 2D Gatch Game Project/Assets/Scripts/HatController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatController : MonoBehaviour
+{
+
+    public Camera cam;
+
+	void Start ()
+    {
+		if(cam == null)
+        {
+            cam = Camera.main;
+        }
+	}
+	
+	// Update is called once per physics timestep
+	void FixUpdate ()
+    {
+        Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
