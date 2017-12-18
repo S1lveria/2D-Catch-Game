23025d481e252cd66061189867e9028530079dc6
Ivan <S1lveria@users.noreply.github.com>
2017-12-17 21:34:08 -0500

    Move the hat
    - Stick the hat position to the input.Mouse position
    - Clamp the hat within the boundaries of a screen
    - Fix the small bug with the position of upper part of the hat

--- 2D Gatch Game Project/Assets/Scripts/HatController.cs
+++ 2D Gatch Game Project/Assets/Scripts/HatController.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 public class HatController : MonoBehaviour
 {
-
     public Camera cam;
+	private Rigidbody2D rb;
+	private float maxWidth;
 
 	void Start ()
     {
+		rb = GetComponent<Rigidbody2D>();
+
 		if(cam == null)
         {
             cam = Camera.main;
         }
+
+		Vector3 upperCorner = new Vector3 (Screen.width, Screen.height, 0.0f);
+		Vector3 targetWidth = cam.ScreenToWorldPoint (upperCorner);
+		maxWidth = targetWidth.x;
 	}
 	
 	// Update is called once per physics timestep
-	void FixUpdate ()
+	void FixedUpdate ()
     {
-        Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 rawPosition = cam.ScreenToWorldPoint (Input.mousePosition);
+        Vector3 targetPosition = new Vector3 (rawPosition.x, 0.0f, 0.0f);
+		float targetWidth = Mathf.Clamp (targetPosition.x, -maxWidth, maxWidth);
+		targetPosition = new Vector3 (targetWidth, targetPosition.y, targetPosition.z);
+		rb.MovePosition (targetPosition);
     }
 }
