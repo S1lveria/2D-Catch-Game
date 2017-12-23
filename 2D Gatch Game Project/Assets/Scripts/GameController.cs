01c6b3827269736aa663ba2c6e9e5d90a5aa9762
Ivan <S1lveria@users.noreply.github.com>
2017-12-22 22:06:20 -0500

    Creating GameController

---
+++ 2D Gatch Game Project/Assets/Scripts/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	public Camera cam;
+	private float maxWidth;
+
+	void Start ()
+	{
+		if(cam == null)
+		{
+			cam = Camera.main;
+		}
+		Vector3 upperCorner = new Vector3 (Screen.width, Screen.height, 0.0f);
+		Vector3 targetWidth = cam.ScreenToWorldPoint (upperCorner);
+		float hatWidth = GetComponent<Renderer>().bounds.extents.x;
+		maxWidth = targetWidth.x - hatWidth;
+
+	}
+}
